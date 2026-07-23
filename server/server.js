abf9c5f24bbb14ccdf23adc8b066289406c275d7
abhishek-0827 <abhicodemaker@gmail.com>
2026-07-23 17:07:58 +0530

    a

--- server/server.js
+++ server/server.js
@@ -146,4 +146,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
-// Trigger reload for new query fields - env file added
+// Trigger reload for new query fields - env file non-srv update
